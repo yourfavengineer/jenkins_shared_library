23a8f43c2e38a9de5a0244da3f1122fb9450cc59
yourfavengineer <kendall8584@yahoo.com>
2024-02-07 00:01:14 -0800

    edit

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -1,5 +1,5 @@
 def call() {
     withSonarQubeEnv('sonar-server') {
-        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Youtube -Dsonar.projectKey=Youtube '''
+        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube-project -Dsonar.projectKey=youtube-project '''
     }
 }
