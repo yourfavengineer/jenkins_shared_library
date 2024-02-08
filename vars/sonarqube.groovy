60ebbb728f80b9b5359fe6066c37c640c66eb270
yourfavengineer <kendall8584@yahoo.com>
2024-02-07 16:02:05 -0800

    configurations

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -1,5 +1,5 @@
 def call() {
     withSonarQubeEnv('sonar-server') {
-        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube-project -Dsonar.projectKey=youtube-project '''
+        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube -Dsonar.projectKey=youtube'''
     }
 }
